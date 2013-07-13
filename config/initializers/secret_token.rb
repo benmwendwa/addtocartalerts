4d52346465f62557113765a26a89d61c0c2a21bc
benjamin <benja.tush@gmail.com>
2013-07-13 11:19:55 +0300

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Addtocartalerts::Application.config.secret_key_base = 'b5d4743be2ae38767a859bc469d6a3ec3270b508a6fe922efc7aa073f57efdc5b2d008a99fc16224bc008adc479c637b08c9d18f8fdcbba76c17568975cea1dc'
